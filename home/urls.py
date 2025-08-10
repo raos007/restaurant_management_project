c1fe24c862fbd2cd316436b8f0213c50fa1207f4
raos007 <pranithkumarponugoti@gmail.com>
2025-08-10 12:03:18 +0530

    25751

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import *
+from django.conf.urls import handler404
+from.import views
 
 urlpatterns = [
     
 ]
+
+handler404 = 'your_project_name.views.custom_404'
