cfd920dde906805346b4bdae6e0d2b73d22db849
raos007 <pranithkumarponugoti@gmail.com>
2025-10-24 18:23:53 +0530

    56090

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class ContactFormSubmission(models.Model):    
+    name = models.CharField(max_length=100)    
+    email = models.EmailField()    
+    message = models.TextField()    
+    created_at = models.DateTimeField(auto_now_add=True)    
+    def _str_(self):        
+        return f"{self.name} - {self.email}"
 # Create your models here.
